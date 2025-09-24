6aa7600e246f57a93d94dbc74d66da5a81e6c8ea
alisoncoderr-cyber <alisoncoderr@gmail.com>
2025-09-24 20:57:48 -0300

    Update celular.py

--- celular.py
+++ celular.py
@@ -1,19 +1,14 @@
 class Celular:
-    marca = "..."
-    modelo = "..."
-
+ 
     def definir_marca(self, nova_marca):
         self.marca = nova_marca
 
     def definir_modelo(self, novo_modelo):
         self.modelo = novo_modelo
+        
+celular = Celular()
+celular.definir_marca("Samsung")
+celular.definir_modelo("Galaxy S23")
 
-
-meu_celular = Celular()
-meu_celular.definir_marca("Samsung")
-meu_celular.definir_modelo("Galaxy S23")
-
-print("Marca:", meu_celular.marca)
-print("Modelo:", meu_celular.modelo)
-
-
+print("Marca:", celular.marca)
+print("Modelo:", celular.modelo)
