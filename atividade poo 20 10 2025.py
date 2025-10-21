9ae1efabceba74028d577daa8058be49a2e38b97
alisoncoderr-cyber <alisoncoderr@gmail.com>
2025-10-20 21:03:03 -0300

    Add files via upload

---
+++ atividade poo 20 10 2025.py
@@ -0,0 +1,36 @@
+#1- questão
+numeros = [10, 20, 30, 40, 50]
+print(numeros)
+
+#2- questâo
+
+numeros = [10, 20, 30, 40, 50]
+
+print("Primeiro elemento:", numeros [0])
+
+print("Último elemento:", numeros [-1])
+
+print("Elemento do meio:", numeros [2])
+
+
+#3- questão
+
+numeros[1] = 25
+print("Lista atualizada:", numeros)
+
+#4- Questão
+
+novo_numero = int(input("Digite um número para adicionar à lista: "))
+numeros.append(novo_numero)
+
+print("Lista atualizada com o novo número:", numeros)
+
+#5- questão
+
+numeros.remove(30)  
+
+print("Lista após remoção:", numeros)
+
+
+
+
